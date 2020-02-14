127b997331ecf438bd66742ab3c51fee3957ef4d
Josh <dash219@gmail.com>
2020-02-14 10:02:15 -0800

    Added files? I think?

--- CamelUpAI/Assets/Scripts/GameMaster.cs
+++ CamelUpAI/Assets/Scripts/GameMaster.cs
@@ -7,12 +7,12 @@ public class GameMaster : ScriptableObject
     public Dictionary<string, Camel> camelList = new Dictionary<string, Camel>();   //stores all the camels so it can call things like cameList["red"].position
     public Dictionary<string, Player> playerList = new Dictionary<string, Player>();//stores all players and their data (e.g. how many coins they have, what cards they have left, what tickets they have)
     public List<TicketPile> ticketPiles = new List<TicketPile>();                   //contains a list of all five ticket piles, each of which contain tickets
-    //public List<Die> diceList;                                                    //list that knows what dice have been rolled so far + all of the dice in the game
+    public List<Die> diceList;                                                    //list that knows what dice have been rolled so far + all of the dice in the game
     public CardPile winnerCards;                                                    //stores cards played to winner bet pile. Cards are a defined object type with attributes (e.g. player, color)
     public CardPile loserCards;                                                     //similar to winnercards    
     public TicketPile RollChips;
-    //public Pyramid gamePyramid;
-    //public Board gameBoard;
+    public Pyramid gamePyramid;
+    public Board gameBoard;
 
     // Start is called before the first frame update
     void Start()
